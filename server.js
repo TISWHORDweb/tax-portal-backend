bec309effc4d3f1b1b24396cb2a140a64d4c75f8
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2025-03-09 18:58:21 -0700

    cors config ***

--- server.js
+++ server.js
@@ -27,6 +27,13 @@ cloudinary.config({
 
 // Middleware
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
